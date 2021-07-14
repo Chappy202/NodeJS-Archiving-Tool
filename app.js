d028e9adca38ada9fb07d0c1eaade82d5feee4a7
JJ <jj@oxibux.com>
2021-07-14 13:45:55 +0200

    Initial Question
    Add initial question

--- app.js
+++ app.js
@@ -3,7 +3,29 @@ require('dotenv').config();
 /*
   > Modules
 */
+// Libraries
+const inquirer = require('inquirer');
+
 // Utils
 const initialMessage = require('./util/initialMessage');
 
+/*
+  > Application
+*/
+// Initial display message when app is started
 initialMessage();
+
+// Initial Question and logic
+inquirer
+    .prompt([{
+        type: 'list',
+        name: 'action',
+        message: `Would you like to 'Compress Folders' or 'Extract Files'?`,
+        choices: [
+            'Compress Folders',
+            'Extract Files'
+        ],
+    }])
+    .then((answers) => {
+        console.log(JSON.stringify(answers, null, ' '));
+    })
