49c74d1b2da7d8eb64f57e38dd685da15498777f
JJ <jj@oxibux.com>
2021-07-14 18:18:51 +0200

    Compression, ENV
    Add introductory compression logic
    Add .env file setting

--- handlers/compress.js
+++ handlers/compress.js
@@ -1,21 +1,33 @@
 const adm = require('adm-zip');
-const winrar = require('winrarjs');
-const seven = require('node-7z');
+const Winrar = require('winrarjs');
+const Seven = require('node-7z');
 
 export default class Delete {
 
-    compressZip(location, target) {
-
+    compressZip(location, target, file) {
+        const zip = new adm();
+        zip.addFile(file);
+        zip.writeZip(target); // Has callback functionality
     }
 
-    compressRar() {
+    compressRar(location, target, file) {
         // Since the rar format is closed-source, the rar drivers need to be installed in order to use this function
+        const winrar = new Winrar();
+        winrar.addFile(file);
+        winrar.setOutput(target);
+        winrar.setConfig({
+            level: process.env.RAR_COMPRESSION || 2
+        });
 
+        winrar.rar(); // Has callback functionality
     }
 
-    compress7z() {
+    compress7z(name, target, file) {
         // The 7-zip libraries and binaries will need to be installed in order for this function to work
-
+        const stream = Seven.add(name, file, {
+            recursive: true,
+            outputDir: target
+        })
     }
 
 }
