8679be4ae9a2eaf9fc747161313fdc4acb04b13c
Gustaf Delport <gustafdelport@gmail.com>
2021-07-18 18:20:14 +0200

    Extract test

--- util/logic/handleExtraction.js
+++ util/logic/handleExtraction.js
@@ -3,12 +3,47 @@ const deleteFile = require('../../lib/deleteFile');
 
 // Handlers
 const Extract = require('../../handlers/extract');
+const fs = require('fs');
+const fileType = require('file-type');
 
 class Extraction {
+
+    
+
     initial(out, filepath) {
-        console.log(out);
-        Extract.extractZip(out, filepath);
-        deleteFile(filepath);
+        const stream = fs.createWriteStream(filepath);
+        const type = await fileType.fromStream(stream)
+
+        switch (type.ext) {
+            case 'zip':
+                {
+                    Extract.extractZip(out,filepath)
+                }
+                break;
+
+            case 'rar':
+                {
+                    console.log("Winrar Version");
+                }
+            break;
+
+            case '7z':
+                {
+                    console.log("7Z Version");
+                }
+            break;
+        
+            default:
+                {
+                    console.log(`${type.ext} is not supported`);
+                }
+                break;
+        }
+
+
+        // console.log(out);
+        // Extract.extractZip(out, filepath);
+        // deleteFile(filepath);
     }
 }
 
